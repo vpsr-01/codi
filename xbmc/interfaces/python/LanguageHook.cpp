24f972ff3713f5978fb8c67d2c1c05a61e7e47e1
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-06-06 21:37:55 +0530

    python3 implementation
    * Updated all the files from old function names to new function names as
      given in pydocs - http://py3c.readthedocs.io/en/latest/reference.html
    * changed char to wchar_t
    * File pointer (FILE *) updated
    * Updated ob_type to be called by multi layered PyObject
    * Removed tp_compare since it's depreciated
    * Updated definitions for InitModule
    * Updated the Python3 Android Script
    * Updated the GIL acquisition and release statements
    * Updated module initialization from init<module> to PyInit_<module> and added the new modules to built-in library through PyImport_AppendInitTab
    * Charset Conversion From char To wchar_t
    * Updated Python WSGI Invoker

--- xbmc/interfaces/python/LanguageHook.cpp
+++ xbmc/interfaces/python/LanguageHook.cpp
@@ -125,7 +125,7 @@ namespace XBMCAddon
       // from the global dictionary
       PyObject* pyid = PyDict_GetItemString(global_dict, "__xbmcaddonid__");
       if (pyid)
-        return PyString_AsString(pyid);
+        return PyUnicode_AsUTF8(pyid);
       return "";
     }
 
@@ -140,7 +140,7 @@ namespace XBMCAddon
       // from the global dictionary
       PyObject* pyversion = PyDict_GetItemString(global_dict, "__xbmcapiversion__");
       if (pyversion)
-        return PyString_AsString(pyversion);
+        return PyUnicode_AsUTF8(pyversion);
       return "";
     }
 
