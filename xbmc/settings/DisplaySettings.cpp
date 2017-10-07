will-districtnerds/xbmc

[osx/displaysettings] - call updateresolutions when "blank other displays" setting is changed for osx aswell

xbmc/settings/DisplaySettings.cpp
@@ -326,7 +326,7 @@ bool CDisplaySettings::OnSettingChanging(std::shared_ptr<const CSetting> setting
 
     return true;
   }
-#if defined(HAS_GLX)
+#if defined(HAS_GLX) || defined(TARGET_DARWIN_OSX)
   else if (settingId == CSettings::SETTING_VIDEOSCREEN_BLANKDISPLAYS)
   {
     g_Windowing.UpdateResolutions();

vpsr-01/codi

xbmc/settings/DisplaySettings.cpp
@@ -326,7 +326,7 @@ bool CDisplaySettings::OnSettingChanging(std::shared_ptr<const CSetting> setting
 
     return true;
   }
-#if defined(HAVE_X11)
+#if defined(HAVE_X11) || defined(TARGET_DARWIN_OSX)
   else if (settingId == CSettings::SETTING_VIDEOSCREEN_BLANKDISPLAYS)
   {
     g_Windowing.UpdateResolutions();